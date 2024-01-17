7ce208ef633439953eebd376429907fa810dfa1b
suby00 <suby00@ejbt.co.kr>
2024-01-17 10:28:24 +0900

    shp파일 Acess

--- src/main/java/com/example/geomweatherapi/ShpReader.java
+++ src/main/java/com/example/geomweatherapi/ShpReader.java
@@ -1,53 +1,38 @@
 package com.example.geomweatherapi;
 
-import org.geotools.data.shapefile.files.ShpFiles;
-import org.geotools.data.shapefile.shp.ShapefileException;
-import org.geotools.data.shapefile.shp.ShapefileReader;
-import org.locationtech.jts.geom.GeometryFactory;
+import org.geotools.data.DataStore;
+import org.geotools.data.DataStoreFinder;
+import org.geotools.data.FeatureSource;
+import org.geotools.feature.FeatureCollection;
+import org.geotools.feature.FeatureIterator;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.filter.Filter;
 
+import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
+import java.util.HashMap;
 
 public class ShpReader {
     public static void main(String[] args) throws IOException {
-//        ClassPathResource shpFile = new ClassPathResource("ctprvn.shp");
-//        shpFile.getFile();
-//        File file = new File("ctprvn.shp");
-//        HashMap<String, Object> map = new HashMap<>();
-//        map.put("url", file.toURI().toURL());
-//
-//        DataStore dataStore = DataStoreFinder.getDataStore(map);
-//        String typeName = dataStore.getTypeNames()[0];
-//
-//        FeatureSource<SimpleFeatureType, SimpleFeature> source = dataStore.getFeatureSource(typeName);
-//        Filter filter = Filter.INCLUDE; // ECQL.toFilter("BBOX(THE_GEOM, 10,20,30,40)")
-//
-//        FeatureCollection<SimpleFeatureType, SimpleFeature> collection = source.getFeatures(filter);
-//        try (FeatureIterator<SimpleFeature> features = collection.features()) {
-//            while (features.hasNext()) {
-//                SimpleFeature feature = features.next();
-//                System.out.print(feature.getID());
-//                System.out.print(": ");
-//                System.out.println(feature.getDefaultGeometryProperty().getValue());
-//            }
-//        }
-        ShapefileReader shapefileReader  = null;
-        try{
-            ShpFiles shpFiles = new ShpFiles("C:\\Users\\bsa05\\workspaces\\geom-weather-api\\src\\main\\resources\\ctprvn.shp");
-            GeometryFactory geometryFactory = new GeometryFactory();
-            shapefileReader = new ShapefileReader(shpFiles, true, false, geometryFactory);
+        File file = new File("src/main/resources/ctprvn.shp");
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("url", file.toURI().toURL());
 
-            while(shapefileReader.hasNext()){
-                ShapefileReader.Record record = shapefileReader.nextRecord();
-                System.out.println(record.toString());
+        DataStore dataStore = DataStoreFinder.getDataStore(map);
+        String typeName = dataStore.getTypeNames()[0];
+
+        FeatureSource<SimpleFeatureType, SimpleFeature> source = dataStore.getFeatureSource(typeName);
+        Filter filter = Filter.INCLUDE; // ECQL.toFilter("BBOX(THE_GEOM, 10,20,30,40)")
+
+        FeatureCollection<SimpleFeatureType, SimpleFeature> collection = source.getFeatures(filter);
+        try (FeatureIterator<SimpleFeature> features = collection.features()) {
+            while (features.hasNext()) {
+                SimpleFeature feature = features.next();
+                System.out.print(feature.getID());
+                System.out.print(": ");
+                System.out.println(feature.getDefaultGeometryProperty().getValue());
             }
-
-        } catch (MalformedURLException e) {
-            throw new RuntimeException(e);
-        } catch (ShapefileException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
     }
 }
